1713e0ed99bec99083fe8a84c0e8bbfbf9817cd2
Jie Yu <yujie.jay@gmail.com>
2014-09-01 22:11:37 -0700

    Added setup script for tmux.

---
+++ tmux/setup.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+
+# Make sure that tmux is installed.
+if ! type tmux > /dev/null 2>&1; then
+  echo "Please install tmux first!"
+  exit 1
+fi
+
+
+# Confirm with the user that we will replace .tmux.conf.
+if [ -f `echo ~/.tmux.conf` ]; then
+  read -p "Are you sure you want to replace ~/.tmux.conf [y/n]? " choice
+  case "$choice" in
+    y|Y ) ;;
+    n|N ) exit 1;;
+    * ) exit 1;;
+  esac
+fi
+
+
+BASE=$(dirname $0)
+if [ $BASE = "." ]; then
+  BASE=$(pwd)
+fi
+
+
+set -eux -o pipefail
+
+
+# Configure tmux.
+rm -f ~/.tmux.conf
+cp $BASE/tmux.conf ~/.tmux.conf
